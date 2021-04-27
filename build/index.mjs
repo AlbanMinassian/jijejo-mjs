37f151c9084a1a38b45446c4793dcd69a3610ef5
AlbanMinassian <alban.minassian@free.fr>
2021-04-27 17:49:33 +0200

    chore: delete old files

--- build/index.mjs
+++
@@ -1,7 +0,0 @@
-import jsonin from './jsonin.mjs';
-import jsonerr from './jsonerr.mjs';
-import jsonout from './jsonout.mjs';
-import jicheck from './jicheck.mjs';
-import jecheck from './jecheck.mjs';
-import jocheck from './jocheck.mjs';
-export { jsonin, jsonerr, jsonout, jicheck, jecheck, jocheck };
--- build/is_object.mjs
+++
@@ -1,4 +0,0 @@
-export default function is_object(optin) {
-  // https://stackoverflow.com/questions/8511281/check-if-a-value-is-an-object-in-javascript
-  return !!optin && optin.constructor === Object;
-}
